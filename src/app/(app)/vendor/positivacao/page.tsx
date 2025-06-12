37678f5805838fab2acaf53eae1d32d607ff079c
Alcides Rede Hiperfarma <alcides.redefarma@gmail.com>
2025-06-12 14:05:55 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <CardContent class

--- src/app/(app)/vendor/positivacao/page.tsx
+++ src/app/(app)/vendor/positivacao/page.tsx
@@ -159,11 +159,10 @@ export default function VendorPositivacaoPage() {
                   <StoreIcon className="h-6 w-6 text-primary" />
                   {store.name} ({store.code})
                 </CardTitle>
-                {/* CardDescription removida daqui */}
               </CardHeader>
               <CardContent className="flex-grow">
                 <p className="text-sm text-muted-foreground mb-2">
-                  Interaja com esta loja para reconhecer sua participação e esforços.
+                  Confirme a positivação dessa loja.
                 </p>
               </CardContent>
               <CardFooter>
