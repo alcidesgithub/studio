41b534f0ec793aa726cd3cd4f57e2a1e83991f13
Alcides Rede Hiperfarma <alcides.redefarma@gmail.com>
2025-06-12 17:24:26 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Button className=

--- src/app/(app)/vendor/positivacao/page.tsx
+++ src/app/(app)/vendor/positivacao/page.tsx
@@ -187,6 +187,8 @@ export default function VendorPositivacaoPage() {
                             <Image
                               src={currentVendorCompany.logoUrl}
                               alt={`Logo ${currentVendorCompany.name}`}
+                              width={60}
+                              height={30}
                               layout="fill"
                               objectFit="contain"
                             />
