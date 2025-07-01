f6df4f8a736d64bdf15410a5658d7784b20bc9fe
Alcides Rede Hiperfarma <alcides.redefarma@gmail.com>
2025-07-01 14:11:22 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,10 +1,8 @@
 
 export default function AuthLayout({
-  children,
-  params,
+  children
 }: {
   children: React.ReactNode;
-  params: { [key: string]: string | string[] | undefined }; // Reverted to a more general type
 }) {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-primary p-4">
